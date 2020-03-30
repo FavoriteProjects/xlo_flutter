78a7327ee6e319363027c0e62c78db6b6f6ef932
Robson <robsonsilv410@gmail.com>
2020-03-30 11:46:40 -0300

    :construction: search bar

---
+++ lib/screens/home/widgets/search_dialog.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+class SearchDialog extends StatefulWidget {
+  final String currentSearch;
+
+  const SearchDialog({this.currentSearch});
+
+  @override
+  _SearchDialogState createState() => _SearchDialogState(currentSearch);
+}
+
+class _SearchDialogState extends State<SearchDialog> {
+  final TextEditingController _controller;
+
+  _SearchDialogState(String currentSearch)
+      : _controller = TextEditingController(
+          text: currentSearch,
+        );
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        Positioned(
+          top: 2.0,
+          left: 2.0,
+          right: 2.0,
+          child: Card(
+            child: TextField(
+              controller: _controller,
+              textInputAction: TextInputAction.search,
+              autofocus: true,
+              decoration: InputDecoration(
+                border: InputBorder.none,
+                contentPadding: const EdgeInsets.symmetric(
+                  vertical: 15.0,
+                ),
+                prefixIcon: IconButton(
+                  icon: Icon(Icons.arrow_back),
+                  color: Colors.grey.shade700,
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                ),
+                suffixIcon: IconButton(
+                  icon: Icon(Icons.close),
+                  color: Colors.grey.shade700,
+                  onPressed: () {
+                    _controller.clear();
+                  },
+                ),
+              ),
+              onSubmitted: (text) {
+                Navigator.of(context).pop(text);
+              },
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
